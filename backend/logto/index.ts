44612fbec2ad53cf936f8ba61ba7e4bcba09fbb9
norfolk247 <george.popov.2000@gmail.com>
2024-06-16 01:26:55 +0700

    init base logto service

---
+++ backend/logto/index.ts
@@ -0,0 +1,33 @@
+import { LogtoExpressConfig, handleAuthRoutes, withLogto } from '@logto/express'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const config: LogtoExpressConfig = {
+    endpoint: process.env.LOGTOENDPOINT || '',
+    appId: process.env.APPID || '',
+    appSecret: process.env.APPSECRET || '',
+    baseUrl: process.env.BASEURL || '', // Change to your own base URL
+}
+const app = express()
+
+app.use(cookieParser())
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIESECRET || '',
+    cookie: { maxAge: 14 * 24 * 60 * 60 * 1000 }, // In miliseconds
+}))
+
+app.use(handleAuthRoutes(config))
+app.get('/', withLogto(config),(req, res) => {
+    if (!req.user.isAuthenticated) {
+        res.redirect('/logto/sign-in')
+        return
+    }
+    res.json(req.user)
+})
+app.listen(3000)
