46e56b23ecd1aafa472d8ce3e384ff29f2b11738
BuildTools <unconfigured@null.spigotmc.org>
2024-06-25 22:35:52 +0300

    types from logto service

--- backend/post-service/types.d.ts
+++ backend/post-service/types.d.ts
@@ -6,3 +6,19 @@ declare module 'PostTypes' {
         create_date: Date
     }
 }
+declare module 'LogtoServiceTypes' {
+    export type LogtoUser = {
+        sub: string,
+        name: string|null,
+        picture: unknown,
+        updated_at: number,
+        username: string|null,
+        created_at: number
+    }
+    export type LogtoError = {
+        code: string,
+        message: string,
+        error: string,
+        error_description: string
+    }
+}
