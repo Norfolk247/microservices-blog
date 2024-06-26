b455f892ddb029baec0610ff7df5bae3af9181ef
BuildTools <unconfigured@null.spigotmc.org>
2024-06-26 16:17:56 +0300

    change db

--- backend/setup-database/createTables.sql
+++ backend/setup-database/createTables.sql
@@ -1,21 +1,17 @@
 CREATE DATABASE apptest;
 \c apptest;
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE
-);
 
 CREATE TABLE IF NOT EXISTS posts (
     id SERIAL PRIMARY KEY,
     body TEXT NOT NULL,
-    author_id INT NOT NULL REFERENCES users(id),
+    author_id VARCHAR(12) NOT NULL,
     create_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
 CREATE TABLE IF NOT EXISTS comments (
     id SERIAL PRIMARY KEY,
     body TEXT NOT NULL,
-    author_id INT NOT NULL REFERENCES users(id),
+    author_id VARCHAR(12) NOT NULL,
     create_date DATE NOT NULL DEFAULT CURRENT_DATE,
     reply_id INT REFERENCES comments(id)
 );
