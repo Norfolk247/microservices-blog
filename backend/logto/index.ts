5f380e42ff0e17b3143b380be0ddcf121475e597
BuildTools <unconfigured@null.spigotmc.org>
2024-07-13 10:30:00 +0300

    fix logto service

--- backend/logto/index.ts
+++ backend/logto/index.ts
@@ -1,9 +1,11 @@
-import { LogtoExpressConfig, handleAuthRoutes, withLogto } from '@logto/express'
+import {LogtoExpressConfig, handleAuthRoutes, withLogto} from '@logto/express'
 import cookieParser from 'cookie-parser'
 import session from 'express-session'
 import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
-import {tokenVerify} from "./middleware/tokenVerify";
+import {tokenVerify} from "./middleware/tokenVerify"
+import {handleErrors} from "./middleware/handleErrors"
 
 dotenv.config()
 
@@ -20,15 +22,21 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIESECRET || '',
-    cookie: { maxAge: 14 * 24 * 60 * 60 * 1000 }, // In miliseconds
+    cookie: {maxAge: 14 * 24 * 60 * 60 * 1000}, // In miliseconds
 }))
 app.use(handleAuthRoutes(config))
-app.get('/', withLogto({...config,getAccessToken: true}),(req, res) => {
+
+app.get('/', withLogto({...config, getAccessToken: true}), (req, res) => {
     if (!req.user.isAuthenticated) {
         res.redirect('/logto/sign-in')
         return
     }
     res.json(req.user)
 })
-app.post('/verify',tokenVerify)
-app.listen(3000)
+app.post('/verify', tokenVerify)
+
+app.listen(3002, () => console.log('logto service up'))
+
+process.on('uncaughtException', (err) => {
+    console.error(err);
+});
