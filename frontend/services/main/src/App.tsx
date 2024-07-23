fbeb8982a11ba988d32076f174d94a3845c60f5a
BuildTools <unconfigured@null.spigotmc.org>
2024-07-23 12:52:17 +0300

    add wrapper

--- frontend/services/main/src/App.tsx
+++ frontend/services/main/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import {Outlet} from "react-router-dom"
 import {MyAppBar} from "@packages/shared"
+import {Box, Paper} from "@mui/material"
 
 const App: React.FC = () => {
     return (
-        <div>
+        <>
             <MyAppBar/>
-            <Outlet/>
-        </div>
+            <Paper elevation={3} style={{ padding: '20px', margin: '20px', backgroundColor: '#f5f5f5' }}>
+                <Box display="flex" justifyContent="center" alignItems="center" minHeight="83vh">
+                    <Outlet/>
+                </Box>
+            </Paper>
+        </>
     )
 }
 
